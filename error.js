27e3bab8df911d7907cc0929f729f9a2c3b2c280
mskian <veerfun23@gmail.com>
2022-07-04 22:46:28 +0530

    Update API

---
+++ error.js
@@ -0,0 +1,10 @@
+export default function dummydata() {
+    const dummy_json = [
+        {
+          "word": "Not Found",
+          "definition": "Not Found",
+          "pronunciation": "Not Found"
+        }
+      ]
+    return dummy_json;
+};
