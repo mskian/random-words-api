0e3e062c6b7731c27edddf5a74ad2c5d7519b31c
mskian <10300271+mskian@users.noreply.github.com>
2024-06-13 09:28:14 +0530

    update test.js remove usued package

--- test.js
+++ test.js
@@ -2,18 +2,16 @@ import * as cheerio from 'cheerio';
 import nlp from 'compromise';
 import speechPlugin from 'compromise-speech'
 nlp.plugin(speechPlugin)
-import randomUseragent from 'random-useragent';
 import axios from 'axios';
 import dummydata from './error.js';
 
-const rua = randomUseragent.getRandom();
 var wordOfDay = [];
 
     axios({
         method: 'GET',
         url: 'https://randomword.com/',
         headers: {
-            'User-Agent': rua,
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
             'Accept-Encoding': 'identity'
         }
     }).then(function(response) {
